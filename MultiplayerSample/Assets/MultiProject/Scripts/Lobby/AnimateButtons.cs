053cd69e322e2c56d5704b47f699ac007423463c
AnemogiannisAlexandros <alex.anemogiannis@hotmail.com>
2019-12-18 21:41:56 +0200

    Lobby Start

---
+++ MultiplayerSample/Assets/MultiProject/Scripts/Lobby/AnimateButtons.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimateButtons : MonoBehaviour
+{
+    public Material materialInstance;
+    public float xoffsetMulty;
+    public float yOffsetMulty;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        materialInstance = Instantiate(materialInstance);
+        GetComponent<Image>().material = materialInstance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        materialInstance.mainTextureOffset += new Vector2(speed * (Mathf.Sin(materialInstance.mainTextureOffset.x + materialInstance.mainTextureOffset.y) * speed + Time.deltaTime* xoffsetMulty), speed * (Mathf.Sin(materialInstance.mainTextureOffset.x+ materialInstance.mainTextureOffset.y) * speed + Time.deltaTime * yOffsetMulty));
+    }
+}
