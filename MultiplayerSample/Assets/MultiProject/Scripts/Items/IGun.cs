b4d15f8005bc5d1ae6ef45a73f1ecf472b228e97
AnemogiannisAlexandros <alex.anemogiannis@hotmail.com>
2020-01-13 21:00:44 +0200

    Weapon System Start

---
+++ MultiplayerSample/Assets/MultiProject/Scripts/Items/IGun.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IGun
+{
+    void Shoot();
+    void CalculateSpread(float spread);
+    void CalculateRecoil(float recoil);
+    void Reload();
+}
--- MultiplayerSample/Assets/MultiProject/Scripts/Items/Weapon.cs
+++ MultiplayerSample/Assets/MultiProject/Scripts/Items/Weapon.cs
@@ -6,7 +6,23 @@ public class Weapon : Item
 {
     [SerializeField]
     [Range(0, 100)]
+    [Tooltip("The Damage of each bullet")]
     protected float damageOfBullets;
+    [SerializeField]
+    [Tooltip("How Often the gun can shoot")]
+    protected float fireRate;
+    [SerializeField]
+    [Tooltip("The gun's Clip Size")]
+    protected int clipSize;
+    [SerializeField]
+    [Tooltip("The current bullets in our clip")]
+    protected int bulletCount;
+    [SerializeField]
+    [Tooltip("The amount of bullet spread applied")]
+    protected float spreadAmount;
+    [SerializeField]
+    [Tooltip("The amount of recoil applied after firing")]
+    protected float recoilAmount;
 
     public Weapon(ItemTiers tier, string ItemName)
     {
@@ -21,13 +37,56 @@ public class Weapon : Item
         base.Init(tier, ItemName);
         itemType = ItemTypes.Weapon;
     }
-
+    #region Getters
     public float GetDamage()
     {
         return damageOfBullets;
     }
+    public float GetFireRate() 
+    {
+        return fireRate;
+    }
+    public int GetClipSize() 
+    {
+        return clipSize;
+    }
+    public int GetBulletCount() 
+    {
+        return bulletCount;
+    }
+    public float GetSpreadAmount() 
+    {
+        return spreadAmount;
+    }
+    public float GetRecoilAmount() 
+    {
+        return recoilAmount;
+    }
+    #endregion Getters
+    #region Setters
     public void SetDamage(float amount)
     {
         damageOfBullets = amount;
     }
+    public void SetFireRate(float amount) 
+    {
+        fireRate = amount;
+    }
+    public void SetClipSize(int amount)
+    {
+        clipSize = amount;
+    }
+    public void SetBulletCount(int amount)
+    {
+        bulletCount = amount;
+    }
+    public void SetSpreadAmount(float amount)
+    {
+        spreadAmount = amount;
+    }
+    public void SetRecoilAmount(float amount)
+    {
+        recoilAmount = amount;
+    }
+    #endregion Setters
 }
